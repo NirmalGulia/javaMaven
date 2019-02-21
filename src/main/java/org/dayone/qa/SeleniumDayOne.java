90d895a06e31a9cb4362ba6f46568c9dec54ec7e
NirmalGulia <nirmal.gulia@gmail.com>
2019-02-20 22:45:13 -0800

    First git

--- src/main/java/org/dayone/qa/SeleniumDayOne.java
+++ src/main/java/org/dayone/qa/SeleniumDayOne.java
@@ -1,25 +1,63 @@
-    package org.dayone.qa;
+package org.dayone.qa;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumDayOne {
+	static WebDriver driver;
+	@BeforeClass
+	public static void browse()
+	
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		
+		driver = new ChromeDriver();
+	}
+	@Test
+	public void test() {
 
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void test1() {
+		System.out.println("Test2");
+
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test3");
+		
+		
+	}
+
+	@Before
+	public void launch() {
+		
+		
 		driver.get("http://www.amazon.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		//driver.navigate().forward();
-		//driver.navigate().back();
+
+		// driver.navigate().forward();
+		// driver.navigate().back();
 		driver.navigate().refresh();
-		driver.close();
-		
 	}
 	
+
+	@AfterClass
+	public static void terminate() {
+		driver.close();
+		System.out.println("After method");
+	}
 
 }
